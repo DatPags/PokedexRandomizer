4efb16453da60d09835d6d46d83ceaddca409504
Michael Pagliaro <mpagliaro98@yahoo.com>
2022-02-27 19:38:57 -0500

    Display unknown image for pokemon not added yet

--- PokedexRandomizer/PkmnInfoRetriever.vb
+++ PokedexRandomizer/PkmnInfoRetriever.vb
@@ -4,6 +4,7 @@
     Private Const URL_IMG_PRE = "https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/"
     Private Const URL_IMG_FEMALE = "female/"
     Private Const URL_IMG_POST = ".png"
+    Private Const URL_IMG_UNKNOWN = "https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/unknown.png"
     Private Const URL_IMG_JSON = "https://raw.githubusercontent.com/msikma/pokesprite/master/data/pokemon.json"
 
     Private Shared _imageJson As Newtonsoft.Json.Linq.JObject
@@ -44,12 +45,33 @@
 
 #Region "Images"
     Private Async Function Get_All_Pkmn_Images(pkmnInfo As PkmnInfo) As Task(Of List(Of BitmapImage))
-        Dim imgList = New List(Of BitmapImage)
-        Dim baseName As String = _imageJson.SelectToken(pkmnInfo.number.ToString("D3")).SelectToken("slug").SelectToken("eng").ToString
-        Dim formsToken = _imageJson.SelectToken(pkmnInfo.number.ToString("D3")).SelectToken("gen-8").SelectToken("forms")
-        Dim forms = formsToken.Children.ToList
+        Dim imgList As New List(Of BitmapImage), baseName As String = "", formsToken As Newtonsoft.Json.Linq.JToken = Nothing, forms As New List(Of Newtonsoft.Json.Linq.JToken)
+        Dim imgUnknown As Boolean = False
+        Try
+            imgList = New List(Of BitmapImage)
+            baseName = _imageJson.SelectToken(pkmnInfo.number.ToString("D3")).SelectToken("slug").SelectToken("eng").ToString
+            formsToken = _imageJson.SelectToken(pkmnInfo.number.ToString("D3")).SelectToken("gen-8").SelectToken("forms")
+            forms = formsToken.Children.ToList
+        Catch ex As Exception
+            ' Pokedex number is not in the list
+            imgUnknown = True
+        End Try
 
         For formIndex = 0 To pkmnInfo.forms.Count - 1
+            ' Unknown image for Pokemon not added yet
+            If imgUnknown Then
+                If imgList.Count = 0 Then
+                    Try
+                        imgList.Add(Await Get_Pkmn_Image_Unknown())
+                    Catch ex As System.Net.WebException
+                        imgList.Add(Nothing)
+                    End Try
+                Else
+                    imgList.Add(imgList(0))
+                End If
+                Continue For
+            End If
+
             ' Special cases for certain Pokemon
             If pkmnInfo.name = "Urshifu" Then
                 Try
@@ -197,13 +219,26 @@
                     End Try
                 Else
                     If imgList.Count = 0 Then
+                        Dim fail As Boolean = False
                         Try
                             imgList.Add(Await Get_Pkmn_Image(baseName))
                         Catch ex As System.Net.WebException
-                            imgList.Add(Nothing)
+                            fail = True
                         End Try
+
+                        If fail Then
+                            Try
+                                imgList.Add(Await Get_Pkmn_Image_Unknown())
+                            Catch ex As System.Net.WebException
+                                imgList.Add(Nothing)
+                            End Try
+                        End If
                     Else
-                        imgList.Add(imgList(0))
+                        Try
+                            imgList.Add(Await Get_Pkmn_Image_Unknown())
+                        Catch ex As System.Net.WebException
+                            imgList.Add(imgList(0))
+                        End Try
                     End If
                 End If
             End If
@@ -213,6 +248,10 @@
 
     Private Async Function Get_Pkmn_Image(imgName As String) As Task(Of BitmapImage)
         Return Await Get_Image_Async(URL_IMG_PRE & imgName & URL_IMG_POST)
+    End Function
+
+    Private Async Function Get_Pkmn_Image_Unknown() As Task(Of BitmapImage)
+        Return Await Get_Image_Async(URL_IMG_UNKNOWN)
     End Function
 #End Region
 
